b52580be4a08576e968c81a03caa4e59a7ae4bbf
Flashk <mevengar@gmail.com>
2021-04-25 21:53:41 +0200

    Start basic design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,16 @@
 import { Component } from '@angular/core';
+import { ThemePalette } from '@angular/material/core';
+
+export interface PeriodicElement {
+  url: string;
+  description: String;
+  enabled: boolean;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {url: "https://hd-olimpo.club/rss/1130.4ff22951d0562feb3b966d7e74172c44", description: 'The Walking Dead', enabled: false},
+  {url: "https://hd-olimpo.club/rss/1130.4ff22951d0562feb3b966d7e74172c44", description: 'The Walking Dead', enabled: true},
+];
 
 @Component({
   selector: 'app-root',
@@ -6,5 +18,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'rss-tracker-web';
+  
+  title = 'RSS Tracker';
+  name = 'Bobea';
+
+  // Slide toggle
+  color: ThemePalette = 'primary';
+  isChecked = false;
+
+  // Table
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  dataSource = ELEMENT_DATA;
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    //this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { FlexLayoutModule } from "@angular/flex-layout";
+
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatTableModule} from '@angular/material/table';
+import {MatIconModule} from '@angular/material/icon';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
@@ -12,7 +23,16 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    FlexLayoutModule,
+    MatGridListModule,
+    MatToolbarModule,
+    MatTableModule,
+    MatIconModule,
+    MatSlideToggleModule,
+    MatButtonModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]

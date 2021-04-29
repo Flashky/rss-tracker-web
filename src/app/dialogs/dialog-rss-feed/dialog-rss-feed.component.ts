5d794c13d460f444d3018b8510843ef6294ba7ec
Flashky <mevengar@gmail.com>
2021-04-29 16:10:11 +0200

    Refactor dialog code

--- src/app/dialogs/dialog-rss-feed/dialog-rss-feed.component.ts
+++ src/app/dialogs/dialog-rss-feed/dialog-rss-feed.component.ts
@@ -1,7 +1,7 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import {MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
 import { RssFeed } from 'src/app/rss-feed';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { RssValidationService } from 'src/app/services/rss-validation.service';
 import { RssFeedValidator } from 'src/app/validators/rss-feed-validator';
 
@@ -14,34 +14,36 @@ export class DialogRssFeedComponent implements OnInit {
 
   // Dialog text labels
   title: string = "";
-  confirmationButtonText: string = "";
+  submitText: string = "";
 
   // Flags
   displaySpinner: boolean = false;
 
-  // FormGroup and form controls:
-  rssFeedFormGroup: FormGroup = new FormGroup({
+  // FormGroup and FormControls - https://fiyazhasan.me/asynchronous-validation-in-angulars-reactive-forms-control/
 
-  });
+  rssFeed: FormGroup = this.formBuilder.group({
 
+    // URL FormControl
+    url: [this.data?.url, { 
+      validators: [ Validators.required ], 
+      asyncValidators: [ RssFeedValidator.valid(this.rssValidationService) ], 
+      updateOn: 'blur' 
+    }],
 
-  // FormControls - https://fiyazhasan.me/asynchronous-validation-in-angulars-reactive-forms-control/
+    // Description FormControl
+    description: [this.data?.description, { 
+      validators: [ Validators.required ]
+    }]
 
-  urlFormControl = new FormControl('', {
-    validators: [ Validators.required ],                              
-    asyncValidators: [ RssFeedValidator.valid(this.rssValidationService) ],
-    updateOn: 'blur' // Update only when focus change to avoid validation spamming on an incomplete url
-  }
-  
-  );
-
-  descriptionFormControl = new FormControl('',  {
-    validators: [ Validators.required ]
   });
 
   constructor(public dialogRef: MatDialogRef<DialogRssFeedComponent>, 
-                @Inject(MAT_DIALOG_DATA) public data: RssFeed,
-                private rssValidationService: RssValidationService) {}
+                @Inject(MAT_DIALOG_DATA) public data: RssFeed = new RssFeed(),
+                private formBuilder: FormBuilder,
+                private rssValidationService: RssValidationService) {
+
+
+  }
 
   ngOnInit(): void {
 
@@ -49,29 +51,37 @@ export class DialogRssFeedComponent implements OnInit {
 
       // Dialog opened in edit mode
       this.title = "Edit RSS feed";
-      this.confirmationButtonText = "Modify";
+      this.submitText = "Modify";
 
     } else {
 
       // Dialog opened in creation mode
       this.data = new RssFeed();
       this.title = "Add new RSS feed";
-      this.confirmationButtonText = "Add";
+      this.submitText = "Add";
     }
     
   }
-
-  isDisabled() {
-    return ((this.data.url == "") || (this.data.description == ""));
-  }
   
-  
-  getButtonColor() {
-    return this.isDisabled() ? "" : "accent";
+  getSubmitButtonColor() {
+    return this.rssFeed.valid ? "accent" : "";
   }
 
-  cancel() {
-    this.dialogRef.close();
+  onSubmit() {
+    
+    // Update result data 
+    this.data.url = this.rssFeed.controls.url.value;
+    this.data.description = this.rssFeed.controls.description.value;
+
+    // TODO perform API POST or PATCH here.
+    // TODO return the resource URI instead of the data itself.
+
+    // Return updated data
+    //this.dialogRef.close(this.data);
+  }
+
+  onCancel() {
+    this.dialogRef.close(null);
   }
 
 }
